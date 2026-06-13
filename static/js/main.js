ff1c11d03d900981740124d2f875661bda214391
qian1yuu <3291615025@qq.com>
2026-06-14 07:11:50 +0800

    Smart email: mailto on mobile, QQ Mail web + clipboard copy on desktop

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,21 @@
+function openEmail() {
+    var toEmail = 'yucheng_furniture@qq.com';
+    var subject = '榆城家具 - 客户咨询';
+    var body = '您好，我对榆城家具的产品感兴趣，请问...';
+    var isMobile = /Android|iPhone|iPad|iPod|webOS/i.test(navigator.userAgent);
+
+    if (isMobile) {
+        // 手机：mailto 直接打开邮件App
+        window.location.href = 'mailto:' + toEmail + '?subject=' + encodeURIComponent(subject) + '&body=' + encodeURIComponent(body);
+    } else {
+        // 电脑：打开QQ邮箱写信页 + 复制邮箱到剪贴板
+        navigator.clipboard.writeText(toEmail).then(function() {
+            alert('收件人邮箱已复制: ' + toEmail + '\n\n请在打开的QQ邮箱中粘贴（Ctrl+V）');
+        }).catch(function() {});
+        window.open('https://mail.qq.com/cgi-bin/frame_html?sid=&r=write', '_blank');
+    }
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     const categoryTabs = document.querySelectorAll('.category-tab');
     const woodTabs = document.querySelectorAll('.wood-tab');
